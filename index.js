e77eee430df647930998b65742bf157c949bae59
jsschf <jsschf@mail.missouri.edu>
2020-04-09 19:11:35 -0500

    added documentation. Final commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*
+  Simple loop that uses modulus operator to return "Fizz" or "Buzz" if
+  'i' is divisible by 3 or 5, respectively, otherwise prints 'i'.
+
+  I also feel this solution is optimal, Big-O complexity is O(n),
+  n in this case is 100
+*/
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
     console.log("Fizz");
